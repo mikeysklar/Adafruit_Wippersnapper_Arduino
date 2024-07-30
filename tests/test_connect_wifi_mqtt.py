264057d6dfe6fa2f032d1ca1b6009adf3abae5ff
brentru <brent@adafruit.com>
2024-07-30 15:00:19 -0400

    add restructured tests

--- tests/test_connect_wifi_mqtt.py
+++ tests/test_connect_wifi_mqtt.py
@@ -2,18 +2,36 @@ import subprocess
 import pytest
 
 
-def test_wifi_ok():
+def test_netfsm_wifi_ssid_not_found():
+    """Test that the Wippersnapper device fails to connect to WiFi due to an invalid SSID in secrets"""
+    # This scenario requires a longer timeout
+    # since it has to fail to connect multiple
+    # times before the test passes
+    result = subprocess.run(
+        [
+            "wokwi-cli",
+            "--elf",
+            "ws-invalid-net-ssid.elf",
+            "--timeout",
+            "55000",
+            "--scenario",
+            "test_netfsm_wifi_ssid_not_found.scenario.yaml",
+        ]
+    )
+    assert result.returncode == 0
+
+def test_netfsm_wifi_invalid_pass():
+    """Test that the Wippersnapper device fails to connect to WiFi due to an invalid network password"""
     # Run the Wokwi CLI
     result = subprocess.run(
         [
             "wokwi-cli",
             "--elf",
-            "wippersnapper_valid_creds.elf",
+            "ws-invalid-net-password.elf",
             "--timeout",
             "30000",
             "--scenario",
-            "test_wifi_ok.scenario.yaml",
+            "test_netfsm_wifi_invalid_pass.scenario.yaml",
         ]
     )
     assert result.returncode == 0
-
