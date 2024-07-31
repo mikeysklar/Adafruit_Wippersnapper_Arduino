84c41438c26b7db86e545efebdc3ac46001a82d7
brentru <brent@adafruit.com>
2024-07-31 16:26:39 -0400

    refactor out the run_wokwi command

--- tests/test_connect_wifi_mqtt.py
+++ tests/test_connect_wifi_mqtt.py
@@ -1,102 +1,48 @@
 # SPDX-FileCopyrightText: 2024 Brent Rubell for Adafruit Industries
 #
 # SPDX-License-Identifier: MIT
-"""Unit tests for WipperSnapper Arduino's network FSM function, runNetFsm()."""
-
+"""Unit tests for WipperSnapper Arduino's network FSM function, runNetFsm."""
 import subprocess
 import pytest
 
-def test_netfsm_establish_wifi_mqtt():
-    """Test that the Wippersnapper device connects to WiFi and MQTT successfully and transitions to the next state."""
+def run_wokwi(elf_file, scenario_file, timeout=30000):
+    """Runs a Wokwi-CLI simulation provided an ELF file, scenario YAML and expected timeout.
+    Returns 0 if successful, non-zero otherwise.
+    """
     result = subprocess.run(
         [
             "wokwi-cli",
             "--elf",
-            f"elf/ws.elf",
+            elf_file,
             "--timeout",
-            "200000",
+            str(timeout),
             "--scenario",
-            "test_netfsm_establish_wifi_mqtt.scenario.yaml",
+            scenario_file,
         ]
     )
-    assert result.returncode == 0
+    return result.returncode
+
+def test_netfsm_establish_wifi_mqtt():
+    """Test that the Wippersnapper device connects to WiFi and MQTT successfully and transitions to the next state."""
+    assert run_wokwi(f"elf/ws.elf","test_netfsm_establish_wifi_mqtt.scenario.yaml", 200000) == 0
 
 def test_netfsm_mqtt_error_auth():
     """Test that the Wippersnapper device fails to connect to the MQTT server due to invalid credentials"""
-    result = subprocess.run(
-        [
-            "wokwi-cli",
-            "--elf",
-            f"elf/ws-mqtt-error-auth.elf",
-            "--timeout",
-            "200000",
-            "--scenario",
-            "test_netfsm_mqtt_error_auth.scenario.yaml",
-        ]
-    )
-    assert result.returncode == 0
+    assert run_wokwi(f"elf/ws-mqtt-error-auth.elf", "test_netfsm_mqtt_error_auth.scenario.yaml", 200000) == 0
 
 def test_netfsm_mqtt_invalid_server():
     """Test that the Wippersnapper device fails to connect to the MQTT server due to an invalid (or offline) server address provided"""
-    result = subprocess.run(
-        [
-            "wokwi-cli",
-            "--elf",
-            f"elf/ws-mqtt-invalid-server.elf",
-            "--timeout",
-            "200000",
-            "--scenario",
-            "test_netfsm_mqtt_invalid_server.scenario.yaml",
-        ]
-    )
-    assert result.returncode == 0
+    assert run_wokwi(f"elf/ws-mqtt-invalid-server.elf", "test_netfsm_mqtt_invalid_server.scenario.yaml", 200000) == 0
 
 def test_netfsm_establish_wifi():
     """Test that the Wippersnapper device connects to WiFi successfully and the FSM transitions
     to the MQTT state."""
-    result = subprocess.run(
-        [
-            "wokwi-cli",
-            "--elf",
-            f"elf/ws-establish-wifi.elf",
-            "--timeout",
-            "50000",
-            "--scenario",
-            "test_netfsm_establish_wifi.scenario.yaml",
-        ]
-    )
-    assert result.returncode == 0
+    assert run_wokwi(f"elf/ws-establish-wifi.elf", "test_netfsm_establish_wifi.scenario.yaml", 50000) == 0
 
 def test_netfsm_wifi_ssid_not_found():
     """Test that the Wippersnapper device fails to connect to WiFi due to an invalid SSID in secrets"""
-    # This scenario requires a longer timeout
-    # since it has to fail to connect multiple
-    # times before the test passes
-    result = subprocess.run(
-        [
-            "wokwi-cli",
-            "--elf",
-            f"elf/ws-invalid-net-ssid.elf",
-            "--timeout",
-            "55000",
-            "--scenario",
-            "test_netfsm_wifi_ssid_not_found.scenario.yaml",
-        ]
-    )
-    assert result.returncode == 0
+    assert run_wokwi(f"elf/ws-invalid-net-ssid.elf", "test_netfsm_wifi_ssid_not_found.scenario.yaml", 55000) == 0
 
 def test_netfsm_wifi_invalid_pass():
     """Test that the Wippersnapper device fails to connect to WiFi due to an invalid network password"""
-    # Run the Wokwi CLI
-    result = subprocess.run(
-        [
-            "wokwi-cli",
-            "--elf",
-            f"elf/ws-invalid-net-password.elf",
-            "--timeout",
-            "55000",
-            "--scenario",
-            "test_netfsm_wifi_invalid_pass.scenario.yaml",
-        ]
-    )
-    assert result.returncode == 0
+    assert run_wokwi(f"elf/ws-invalid-net-password.elf", "test_netfsm_wifi_invalid_pass.scenario.yaml", 55000) == 0
